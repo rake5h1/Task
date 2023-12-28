9e4c19cc8263baefdb9102b340f9fee5593750e1
Rakesh <40319394+rake5h1@users.noreply.github.com>
2023-12-28 10:20:47 +0530

    'changes'

--- src/main/java/com/task/automation/core/DriverManager.java
+++ src/main/java/com/task/automation/core/DriverManager.java
@@ -41,7 +41,7 @@ public class DriverManager {
 				firefoxOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 				firefoxOptions.addArguments("use-fake-ui-for-media-stream");
 				firefoxOptions.addArguments("--headless");
-				// firefoxOptions.setBinary("/opt/firefox/firefox");
+				firefoxOptions.setBinary("/snap/bin/firefox");
 				// firefoxOptions.addPreference("permissions.default.microphone", 1);
 				// firefoxOptions.addPreference("permissions.default.camera", 1);
 				firefoxOptions.addPreference("media.navigator.permission.disabled", true);
